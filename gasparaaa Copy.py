ad25dedf773bbfa2593cadc24b72a881a1634713
Gaspar Arias <garias@splunk.com>
2026-03-03 10:06:02 +0000

    tres

--- gasparaaa Copy.py
+++ gasparaaa Copy.py
@@ -75,6 +75,25 @@ def call_api_1(action=None, success=None, container=None, results=None, handle=N
     ## Custom Code End
     ################################################################################
 
+    call_api_2(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def call_api_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("call_api_2() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
     return
 
 
